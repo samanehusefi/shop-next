23f3252918c81db0cd43ab16dbe4e82eb999e180
samanehyousefi <samanehyousefi64@gmail.com>
2026-09-12 17:21:22 +0330

    Updates

--- _next/static/psMl5T6ckfYEwoDvH2J5j/_buildManifest.js
+++ _next/static/psMl5T6ckfYEwoDvH2J5j/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/shop-next//_next/:path+",
+        "destination": "/shop-next/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
